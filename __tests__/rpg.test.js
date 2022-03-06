47098f83426deb8498c522a5177c862a9cf02651
Yazmin Perez <zoeperez333@gmail.com>
2022-03-06 12:23:35 -0800

    test2-3 passing

--- __tests__/rpg.test.js
+++ __tests__/rpg.test.js
@@ -12,11 +12,38 @@ describe ('Character', () => {
     expect(character.cha).toEqual(0);
     expect(character.wis).toEqual(0);
     expect(character.con).toEqual(0);
-    expect(character.hitPoints).toEqual(0);
+    expect(character.hp).toEqual(0);
     expect(character.level).toEqual(1);  
   });
 
-  test('', () => {
-    
+  describe ('diceRoll', () => {
+
+  test('should return a random number between 1-20', () => {
+    let character = new Character();
+    expect(character.diceRoll()).toBeLessThanOrEqual(20);
+  });
+});
+
+describe('statGenerator', () => {
+
+  test('should return a character function with a set of attributes with random number between 1-20', () => {
+    let character = new Character();
+    character.statGenerator();
+    expect(character.name).toEqual("");
+    expect(character.class).toEqual("");
+    expect(character.str).toBeLessThanOrEqual(20);
+    expect(character.dex).toBeLessThanOrEqual(20);
+    expect(character.int).toBeLessThanOrEqual(20);
+    expect(character.cha).toBeLessThanOrEqual(20);
+    expect(character.wis).toBeLessThanOrEqual(20);
+    expect(character.con).toBeLessThanOrEqual(20);
+    expect(character.hp).toBeLessThanOrEqual(20);
+    expect(character.level).toEqual(1); 
+
+
   })
+})
+
+
+
 });
--- src/rpg.js
+++ src/rpg.js
@@ -8,9 +8,22 @@ export default class Character {
   this.cha = 0;
   this.wis = 0;
   this.con = 0;
-  this.hitPoints = 0;
+  this.hp = 0;
   this.level = 1;
 }
+  
+diceRoll() {
+  return Math.floor(Math.random() * 20 + 1)
+}
 
+statGenerator() {
+  this.str = this.diceRoll();
+  this.dex = this.diceRoll();
+  this.int = this.diceRoll();
+  this.cha = this.diceRoll();
+  this.wis = this.diceRoll();
+  this.con = this.diceRoll();
+  this.hp = this.diceRoll();
+}
 
 }
