9874bda884dd4b4aa5e0a19da610a303b2032b75
Yazmin Perez <zoeperez333@gmail.com>
2022-03-07 18:53:56 -0800

    enemy test2 passing

--- __tests__/enemy.test.js
+++ __tests__/enemy.test.js
@@ -1,6 +1,7 @@
 import Enemy from './../src/enemy.js';
 
 describe ('Enemy', () => {
+  
   test('should return enemy function', () => {
   let enemy = new Enemy();
   expect(enemy.name).toEqual("");
@@ -9,4 +10,13 @@ describe ('Enemy', () => {
   expect(enemy.level).toEqual(1);
   expect(enemy.bonus).toEqual(1);
 });
+
+describe ('classRoll', () => {
+
+  test('should return a random number between 1-4', () => {
+    let enemy = new Enemy();
+    expect(enemy.classRoll()).toBeLessThanOrEqual(4);
+  });
+});
+
 });
